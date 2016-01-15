8ae7f9955fdab328f74ed228b95102d31722faad
Clif Gordon <cgordon@gmail.com>
2016-01-15 00:01:12 -0800

    Scrape The Pine Box

---
+++ scrapers/thePineBox.js
@@ -0,0 +1,29 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+module.exports = {
+    scrapeSite: function(callback) {
+        request('http://www.pineboxbar.com/draft', function(err, res, body) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (res.statusCode != 200) {
+                return callback('Failed to fetch page. Status code: ' + res.statusCode);
+            }
+
+            $ = cheerio.load(body);
+
+            var beers = [];
+            $('td.draft_brewery').each(function(i, element) {
+                var brewery = $(this).text();
+                var beer = $(this).nextAll('td.draft_name').text();
+
+                beers.push(brewery.trim() + ' ' + beer.trim());
+            });
+
+            return callback(null, beers);
+        });
+    }
+};
+
