80356a5aaf3d185bb6eb7c981b4d05200ef0a305
Clif Gordon <cgordon@gmail.com>
2016-01-14 23:14:12 -0800

    Scrape The Sixgill

---
+++ scrapers/theSixgill.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+module.exports = {
+    scrapeSite: function(callback) {
+        request('http://thesixgill.com/beer-cider.html', function(err, res, body) {
+            if (err) {
+                return callback(err);
+            }
+
+            $ = cheerio.load(body);
+
+            beers = [];
+
+            // There are two tables on the page. The first is the draft list.
+            $('tbody').first().children('tr').each(function(i, element) {
+                var children = $(this).children();
+                beers.push($(children[0]).text().trim());
+            });
+
+            return callback(null, beers);
+        });
+    }
+};
+
