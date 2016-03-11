36af5acf4ada613bcad4c6329ba0b833ec280fd2
Clif Gordon <cgordon@gmail.com>
2016-03-10 21:24:46 -0800

    Don't ping slack unless we've failed twice in a row. Fix #36

--- config/secrets.js
+++ config/secrets.js
@@ -19,7 +19,8 @@ module.exports = {
     slack: {
         scraperFailureWebhook: {
             uri: process.env.BEERNOTIFIER_SLACK_SCRAPERFAILUREWEBHOOK_URI,
-            channel: process.env.BEERNOTIFIER_SLACK_SCRAPERFAILUREWEBHOOK_CHANNEL
+            channel: process.env.BEERNOTIFIER_SLACK_SCRAPERFAILUREWEBHOOK_CHANNEL,
+            failureThreshold: 2
         }
     }
 }
--- models/dataSource.js
+++ models/dataSource.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var dataSourceSchema = new Schema({
     scraper: { type: String, required: true },
     scraperStatus: { type: String, trim: true, default: 'ok' },
+    consecutiveFailures: { type: Number, default: 0 },
     isDisabled: { type: Boolean, default: false },
     name: { type: String, trim: true, required: true },
     homeUrl: { type: String, trim: true, required: true},
